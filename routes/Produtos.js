3a5087d62f3e59f487b354081901c464d80e3fe1
Henrique Guimarães <henrique2003.com@gmail.com>
2024-08-29 18:10:35 -0300

    ordenação

--- routes/Produtos.js
+++ routes/Produtos.js
@@ -5,56 +5,32 @@ import upload from '../uploads/multer.js';
 const router = express.Router();
 
 router.get('/products', async (req, res) => {
-    let html = ''
     try {
 
         const products = await Product.find({});
         if (!products?.length) return res.status(204).send();
-        
-        products.forEach(produto => {
-            const valorTotal = produto.price * (1 - produto.promocao / 100)
-            html += `
-            <div class="col-5 col-xl-3 col-xxl-2 btn text-start p-0">
-                <div class="card p-0 align-items-center">
-                    <img class="card-img-produtos" data-bs-toggle="modal" data-bs-target="#modal-5"
-                        alt="${produto.name}" src="http://localhost:3000/${produto.img}">
-                    <div class="card-body card-header ps-2 pt-2 px-1 w-100">
-                        <h5 class="fs-6 titulo-produto fw-bold">${produto.name}</h5>
-                        <ul class="ul-produtos">
-                        <li class="mb-2">Categoria: ${produto.category}</li>
-                        <li class="">${produto.description}</li>
-                        </ul>
-                        <div class="mt-auto d-flex justify-content-between align-items-center w-100">
-                        <div>
-                            <span class="preco-parcelado text-black-50">
-                            10x de ${ valorTotal / 10 }
-                            </span>
-                            <span class="preco-total">
-                            </br>
-                            ${produto.promocao > 0 ? 
-                                `<strong class="preco-promo m fw-light text-decoration-line-through text-black-50">
-                                    R$ ${produto.price.toFixed(2)}
-                                </strong>
-                                </br>` : 
-                                ''}
-                            <strong>
-                                R$
-                            </strong>
-                            ${valorTotal}
-                            </span>
-                        </div>
-                        <a href="#" type="button" class="btn btn-roxo btn-custom">+ <i class="bi bi-cart2"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>`
-        });
-        res.status(200).send(html);
+
+        res.status(200).json(products);
     }
     catch (err) {
-           console.error('Erro ao buscar produtos:', err);
-      }
-      if (!res.headersSent) {
+        console.error('Erro ao buscar produtos:', err);
+    }
+    if (!res.headersSent) {
+        return res.status(500).send('Erro ao buscar produtos');
+    }
+});
+
+router.get('/products/promocao', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        if (!products?.length) return res.status(204).send();
+        const produtosNaPromocao = products.filter(produto => produto.promocao !== null && produto.promocao > 0)
+        res.status(200).json(produtosNaPromocao);
+    }
+    catch (err) {
+        console.error('Erro ao buscar produtos:', err);
+    }
+    if (!res.headersSent) {
         return res.status(500).send('Erro ao buscar produtos');
     }
 })
