f258c8a3a4894be8f7f968dd75f182093db9f1f9
Henrique Guimarães <henrique2003.com@gmail.com>
2024-10-29 17:23:01 -0300

    1.0

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors({
-  origin: "https://grindzone-of.netlify.app/",
+  origin: "https://grindzone-of.netlify.app",
   credentials: true,
 }));
 
