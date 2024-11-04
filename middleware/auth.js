a4e59bed21913a8b58b400324647f8ff193065e6
Henrique Guimarães <henrique2003.com@gmail.com>
2024-11-04 12:16:12 -0300

    ajustando rota check session

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,20 +3,17 @@ import jwt from 'jsonwebtoken';
 const authMiddleware = async (req, res, next) => {
     const token = req.cookies.token;
 
-    // Se o token não existir, ainda assim prosseguir para a rota
     if (!token) {
-        // Ao invés de retornar um erro, vamos passar para o próximo middleware
         return next();
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userId = decoded.userId; // Salva o ID do usuário no request
-        next(); // Continua para o próximo middleware ou rota
+        req.userId = decoded.userId;
+        next();
     } catch (error) {
         console.error('Erro de autenticação:', error);
-        // Se o token for inválido, ainda assim vamos permitir que a verificação de sessão funcione
-        return next();
+        return res.status(403).json({ msg: 'Token inválido ou expirado.' });
     }
 };
 
