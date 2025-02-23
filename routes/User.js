39bb26678405f3f5f5e2391026e43b888c44c098
Henrique010101 <henrique2003.com@gmail.com>
2025-02-23 16:54:27 -0300

    ajustando cookies

--- routes/User.js
+++ routes/User.js
@@ -2,17 +2,17 @@ import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
-import { JWT_SECRET } from '../app.js'
+import { JWT_SECRET } from '../app.js';
 import loginMiddleware from '../middleware/login.js';
 import authMiddleware from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/check-session', authMiddleware, (req, res) => {
-  if (req.userId) { // Verifica se o userId foi definido
-      return res.status(200).json({ isAuthenticated: true });
+  if (req.userId) {
+    return res.status(200).json({ isAuthenticated: true });
   } else {
-      return res.status(200).json({ isAuthenticated: false }); // Retorna false se não estiver autenticado
+    return res.status(200).json({ isAuthenticated: false });
   }
 });
 
@@ -27,18 +27,18 @@ router.get("/logout", authMiddleware, (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-  const { name, email, password, confirmpassword } = req.body
+  const { name, email, password, confirmpassword } = req.body;
 
   const sanitizedEmail = email.trim().toLowerCase();
 
   if (!name) {
-    return res.status(422).json({ msg: 'O nome é obrigatório!' })
+    return res.status(422).json({ msg: 'O nome é obrigatório!' });
   }
   if (!sanitizedEmail) {
-    return res.status(422).json({ msg: 'O email é obrigatório!' })
+    return res.status(422).json({ msg: 'O email é obrigatório!' });
   }
   if (!password) {
-    return res.status(422).json({ msg: 'A senha é obrigatória!' })
+    return res.status(422).json({ msg: 'A senha é obrigatória!' });
   }
   if (password !== confirmpassword) {
     return res.status(422).json({ msg: 'As senhas não conferem!' });
@@ -46,37 +46,37 @@ router.post('/register', async (req, res) => {
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
-  return res.status(422).json({ msg: 'Por favor, insira um email válido!' });
+    return res.status(422).json({ msg: 'Por favor, insira um email válido!' });
   }
 
-  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;  
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
   if (!passwordRegex.test(password)) {
-      return res.status(422).json({ msg: 'A senha deve conter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula e um número!' });
+    return res.status(422).json({ msg: 'A senha deve conter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula e um número!' });
   }
 
-  const userExists = await User.findOne({ email: sanitizedEmail })
+  const userExists = await User.findOne({ email: sanitizedEmail });
 
   if (userExists) {
-    return res.status(422).json({ msg: 'O email já existe!' })
+    return res.status(422).json({ msg: 'O email já existe!' });
   }
 
-  const salt = await bcrypt.genSalt(12)
-  const passwordHash = await bcrypt.hash(password, salt)
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
 
   const user = new User({
     name,
     email: sanitizedEmail,
     password: passwordHash
-  })
+  });
 
   try {
     await user.save();
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '4h' });
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '4h' });
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production' ? true : false,
+      secure: process.env.NODE_ENV === 'production',
       maxAge: 4 * 60 * 60 * 1000,
       sameSite: 'None',
     });
@@ -91,7 +91,7 @@ router.post('/register', async (req, res) => {
       msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!'
     });
   }
-})
+});
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
@@ -116,24 +116,22 @@ router.post('/login', async (req, res) => {
       return res.status(401).send({ message: "Senha incorreta." });
     }
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '4h' });
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '4h' });
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production' ? true : false,
+      secure: process.env.NODE_ENV === 'production',
       maxAge: 4 * 60 * 60 * 1000,
       sameSite: 'None',
     });
 
-    res.status(200).send(
-      {
-        message: "Login bem-sucedido.",
-        user: {
-          email: user.email,
-          name: user.name
-        }
+    res.status(200).send({
+      message: "Login bem-sucedido.",
+      user: {
+        email: user.email,
+        name: user.name
       }
-    );
+    });
   } catch (error) {
     console.error('Erro ao processar login:', error);
     res.status(500).send({ message: "Erro interno do servidor." });
